84c632cb5c4b224afb155b33f2f5fce78935f579
HugoVD <sartos3000@hotmail.es>
2024-10-13 16:06:53 -0600

    Task falta UI

--- Assets/Tbox/Scripts/Objectives/TaskUI.cs
+++ Assets/Tbox/Scripts/Objectives/TaskUI.cs
@@ -1,18 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class TaskUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Transform taskContainer;
+    public GameObject taskPrefab;
 }
