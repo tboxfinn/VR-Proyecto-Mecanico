413f16a1440f05a817e1d14ab5ebb2b05005ab34
Gayorly <a21390050@ucslp.net>
2025-02-25 18:41:41 -0600

    Update cosas extras

---
+++ Assets/Tbox/Scripts/Objectives/Tasks/TasksScripts/TaskBateria.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskBateria : TaskStepController
+{
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+}
